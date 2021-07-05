0f635ffc74e4d17f78144269bede2ede708d5012
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2021-07-05 14:14:48 +0430

    chore: added files left behind

--- example/android/app/src/main/java/com/example/MainActivity.java
+++ example/android/app/src/main/java/com/example/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.reactnativedominantcolor;
+package com.example;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "DominantColorExample";
+    return "example";
   }
 }
